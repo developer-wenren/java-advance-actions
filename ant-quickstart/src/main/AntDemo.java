21193c9b03f68119cd6203cab37c37f0329f85a0
wrcj12138aaa <wrcj12138@163.com>
2018-10-15 22:51:47 +0800

    Initial commit

---
+++ ant-quickstart/src/main/AntDemo.java
@@ -0,0 +1,12 @@
+package main;
+
+/**
+ * @author One
+ * @description
+ * @date 2018/10/14
+ */
+public class AntDemo {
+    public static void main(String[] args) {
+        System.out.println("Hello,Ant");
+    }
+}
